e2268d52b3874e5d79b02d1f45dfb934840328e9
vmsclass <47849571+vmsclass@users.noreply.github.com>
2025-12-31 11:04:03 +0530

    Add files via upload

---
+++ program8.py
@@ -0,0 +1,9 @@
+# program to find the largest number among two numbers
+
+number1 = int(input("Enter number 1 "))
+number2 = int(input("Enter number 2 "))
+
+if number1 > number2:
+    print("Largest number is : ",number1)
+else:
+    print("Largest number is : ",number2)
