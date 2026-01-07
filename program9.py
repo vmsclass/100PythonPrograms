80a007d1ba35164f63e4f4fa80ecb15e04ee8569
vmsclass <47849571+vmsclass@users.noreply.github.com>
2026-01-07 13:13:05 +0530

    Add files via upload

---
+++ program9.py
@@ -0,0 +1,15 @@
+# Finding the largest number among three numbers
+A = int(input("Enter the first number"))
+B = int(input("Enter the second number"))
+C = int(input("Enter the third number"))
+
+if A>B and A>C:
+    print("Largest number is : ",A)
+elif B>C:
+    print("Largest number is : ",B)
+else:
+    print("Largest number is : ",C)
+    
+    
+    
+    
